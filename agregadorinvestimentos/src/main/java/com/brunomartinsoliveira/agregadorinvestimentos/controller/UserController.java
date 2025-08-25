dcaa1e99b772453df78b2124a889eb24aeeb65c0
Bruno Martins <brunomartinsoliveira28@gmail.com>
2025-08-24 23:59:23 -0300

    Projeto iniciado, MYSQL iniciado e configurado, entity criada e tabelas criadas

---
+++ agregadorinvestimentos/src/main/java/com/brunomartinsoliveira/agregadorinvestimentos/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.brunomartinsoliveira.agregadorinvestimentos.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/users")
+public class UserController {
+
+    
+}
+
+
+
+
+
