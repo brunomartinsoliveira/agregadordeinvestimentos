8e92fa1663ddf530761d783840e77d7c7d7002e1
Bruno Martins <brunomartinsoliveira28@gmail.com>
2025-08-25 14:36:58 -0300

    Adicionado listar usuários

--- agregadorinvestimentos/src/main/java/com/brunomartinsoliveira/agregadorinvestimentos/controller/UserController.java
+++ agregadorinvestimentos/src/main/java/com/brunomartinsoliveira/agregadorinvestimentos/controller/UserController.java
@@ -1,33 +1,47 @@
 package com.brunomartinsoliveira.agregadorinvestimentos.controller;
 
 import com.brunomartinsoliveira.agregadorinvestimentos.dto.CreateUserDto;
-import com.brunomartinsoliveira.agregadorinvestimentos.entity.User;
 import com.brunomartinsoliveira.agregadorinvestimentos.service.UserService;
+import com.brunomartinsoliveira.agregadorinvestimentos.entity.User;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
+import java.util.List;
 import java.util.UUID;
 
 @RestController
 @RequestMapping("/v1/users")
 public class UserController {
 
-    private final UserService userService;
+    private UserService userService;
 
     public UserController(UserService userService) {
         this.userService = userService;
     }
 
     @PostMapping
-    public ResponseEntity<Void> createUser(@RequestBody CreateUserDto createUserDto) {
-        UUID userId = userService.createUser(createUserDto);
+    public ResponseEntity<org.apache.catalina.User> createUser(@RequestBody CreateUserDto createUserDto) {
+        var userId = userService.createUser(createUserDto);
+
         return ResponseEntity.created(URI.create("/v1/users/" + userId)).build();
     }
 
     @GetMapping("/{userId}")
     public ResponseEntity<User> getUserById(@PathVariable("userId") String userId) {
-        // lógica para buscar e retornar usuário
-        return null;
+        var user = userService.getUserById(userId);
+
+        if (user.isPresent()) {
+            return ResponseEntity.ok(user.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> listUsers() {
+        var users = userService.listUsers();
+
+        return ResponseEntity.ok(users);
     }
 }
--- agregadorinvestimentos/src/main/java/com/brunomartinsoliveira/agregadorinvestimentos/service/UserService.java
+++ agregadorinvestimentos/src/main/java/com/brunomartinsoliveira/agregadorinvestimentos/service/UserService.java
@@ -1,12 +1,15 @@
 package com.brunomartinsoliveira.agregadorinvestimentos.service;
 
+import java.time.Instant;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
 import com.brunomartinsoliveira.agregadorinvestimentos.dto.CreateUserDto;
 import com.brunomartinsoliveira.agregadorinvestimentos.entity.User;
 import com.brunomartinsoliveira.agregadorinvestimentos.repository.UserRepository;
-import org.springframework.stereotype.Service;
 
-import java.time.Instant;
-import java.util.UUID;
+import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
@@ -21,11 +24,19 @@ public class UserService {
         var entity = new User(
                 createUserDto.username(),
                 createUserDto.email(),
-                createUserDto.password()
-        );
+                createUserDto.password());
 
         var userSaved = userRepository.save(entity);
-
         return userSaved.getUserId();
+    }
+
+    public Optional<User> getUserById(String userId) {
+
+        return userRepository.findById(UUID.fromString(userId));
+    }
+
+    public List<User> listUsers() {
+
+        return userRepository.findAll();
     }
 }
